84e6a3afa299c059793040a1e4709c9a3b08b877
Chandrabhan Singh <chandrabhan.code@outlook.com>
2018-05-17 00:22:50 +0200

    Output text chenged for dotne console appp

--- DotNetConsole/Program.cs
+++ DotNetConsole/Program.cs
@@ -6,9 +6,14 @@ namespace DotNetConsole
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("\n");
             Console.WriteLine("==========Hello Docker enthusiasts!==========");
+            Console.WriteLine("\n");
             Console.WriteLine("I am a .Net Core Console app running inside a container");
-            Console.WriteLine($"My host name is: {System.Net.Dns.GetHostName()}");            
+            Console.WriteLine($"My host name is: {System.Net.Dns.GetHostName()}");
+            Console.WriteLine("\n");
+            Console.WriteLine("=============================================");
+            Console.WriteLine("\n");         
         }
     }
 }

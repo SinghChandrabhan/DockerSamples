c20dca4231adf95a538a36f47c8c6a7a58281194
Chandrabhan Singh <chandrabhan.code@outlook.com>
2018-05-16 21:12:07 +0200

    Sample .NetCore console app for docker

---
+++ DotNetConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DotNetConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==========Hello Docker enthusiasts!==========");
+            Console.WriteLine("I am a .Net Core Console app running inside a container");
+            Console.WriteLine($"My host name is {System.Net.Dns.GetHostName()}");
+            Console.ReadLine();
+        }
+    }
+}
